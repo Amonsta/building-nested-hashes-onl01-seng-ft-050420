7c73e9fc1abfcacdfa236afdc191082fc9bb7555
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-13 03:02:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,14 +1,11 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {:name => "Lord Montague", :age => "53"
-        #your key/value pairs here
+      :patriarch => { :name => "Lord Montague", :age => "53"
         },
       :matriarch => { :name => "Lady Montague", :age => "54"
-        #your key/value pairs here
         },
       :hero => { :name => "Romeo", :age => "15", :status => "alive"
-        #your key/value pairs here
         },
       :hero_friends => []
    }, 
